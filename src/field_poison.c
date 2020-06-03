6041f926a9f73a924e18f8207d0cb259a840f6ae
Shock3600 <dalston.levi.jones@gmail.com>
2020-06-02 20:31:03 -0500

    Removed pokemon that dont take poison damage from taking it in OW

--- src/field_poison.c
+++ src/field_poison.c
@@ -17,6 +17,7 @@
 #include "constants/field_poison.h"
 #include "constants/party_menu.h"
 #include "constants/species.h"
+#include "constants/abilities.h"
 
 static bool32 IsMonValidSpecies(struct Pokemon *pokemon)
 {
@@ -125,7 +126,7 @@ s32 DoPoisonFieldEffect(void)
     u32 numFainted = 0;
     for (i = 0; i < PARTY_SIZE; i++)
     {
-        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN)
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && GetAilmentFromStatus(GetMonData(pokemon, MON_DATA_STATUS)) == AILMENT_PSN && GetMonAbility(pokemon) != ABILITY_POISON_HEAL && GetMonAbility(pokemon) != ABILITY_IMMUNITY && GetMonAbility(pokemon) != ABILITY_MAGIC_GUARD)
         {
             hp = GetMonData(pokemon, MON_DATA_HP);
             if (hp == 1 || --hp == 1)
